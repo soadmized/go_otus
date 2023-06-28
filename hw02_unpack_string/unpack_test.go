14d8f60654eb0954aad60f0b825b00c34b9ae03a
soadmized <soadmized1@gmail.com>
2023-06-28 23:34:26 +0300

    lint fix

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -2,9 +2,9 @@ package hw02unpackstring
 
 import (
 	"errors"
-	"github.com/stretchr/testify/assert"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
