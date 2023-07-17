b9c68a4b246ee859aa1165ffa03b4d85b249114b
soadmized <soadmized1@gmail.com>
2023-07-17 23:08:09 +0300

    hw03: changes according to review

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,68 +1,53 @@
 package hw03frequencyanalysis
 
 import (
-	"regexp"
 	"sort"
+	"strings"
 )
 
 func Top10(text string) []string {
-	space := regexp.MustCompile(`\s`)
-	splitted := space.Split(text, -1)
-
+	splitted := strings.Fields(text)
 	freq := make(map[string]int, 0)
 
 	for _, s := range splitted {
 		if s != "" {
-			freq[s] = freq[s] + 1 //nolint:gocritic
+			freq[s]++
 		}
 	}
 
+	top := []string{}
 	sorted := sortByWordCount(freq)
 	if len(sorted) == 0 {
-		return []string{}
+		return top
 	}
 
-	keys := []string{}
-	for _, s := range sorted {
-		keys = append(keys, s.Key)
+	top = append(top, sorted...)
+
+	return top[:10]
+}
+
+func sortByWordCount(wordFrequencies map[string]int) []string {
+	words := collectWords(wordFrequencies)
+	sort.Slice(words, func(i, j int) bool {
+		curr := wordFrequencies[words[i]]
+		next := wordFrequencies[words[j]]
+
+		if curr == next {
+			return words[i] < words[j]
+		}
+
+		return curr > next
+	})
+
+	return words
+}
+
+func collectWords(wordFreq map[string]int) []string {
+	words := []string{}
+
+	for w := range wordFreq {
+		words = append(words, w)
 	}
 
-	return keys[:10]
-}
-
-func sortByWordCount(wordFrequencies map[string]int) PairList {
-	list := make(PairList, len(wordFrequencies))
-	i := 0
-
-	for k, v := range wordFrequencies {
-		list[i] = Pair{k, v}
-		i++
-	}
-
-	sort.Stable(sort.Reverse(list))
-
-	return list
-}
-
-type Pair struct {
-	Key   string
-	Value int
-}
-
-type PairList []Pair
-
-func (p PairList) Len() int {
-	return len(p)
-}
-
-func (p PairList) Less(i, j int) bool {
-	if p[i].Value == p[j].Value {
-		return p[i].Key > p[j].Key
-	}
-
-	return p[i].Value < p[j].Value
-}
-
-func (p PairList) Swap(i, j int) {
-	p[i], p[j] = p[j], p[i]
+	return words
 }
