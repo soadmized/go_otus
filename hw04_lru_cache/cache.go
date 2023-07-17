04d0556be7b63b19b2002d7aed96534abb45739c
soadmized <soadmized1@gmail.com>
2023-07-17 11:14:08 +0300

    hw04: add Set method

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -3,17 +3,47 @@ package hw04lrucache
 type Key string
 
 type Cache interface {
-	Set(key Key, value interface{}) bool
-	Get(key Key) (interface{}, bool)
+	Set(key Key, value any) bool
+	Get(key Key) (any, bool)
 	Clear()
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*Node
+}
+
+func (l lruCache) Set(key Key, value any) bool {
+	_, exist := l.Get(key)
+	if exist {
+		node := l.items[key]
+		node.Value = value
+		l.queue.MoveToFront(node)
+
+		return true
+	}
+
+	if l.queue.Len() > l.capacity {
+		last := l.queue.Back()
+		l.queue.Remove(last)
+		// TODO: delete(l.items, key???)
+	}
+
+	node := l.queue.PushFront(value)
+	l.items[key] = node
+
+	return false
+}
+
+func (l lruCache) Get(key Key) (any, bool) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l lruCache) Clear() {
+	//TODO implement me
+	panic("implement me")
 }
 
 func NewCache(capacity int) Cache {
