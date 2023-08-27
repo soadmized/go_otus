f6c88977349c797872d477dabdca2e895d33d738
soadmized <soadmized1@gmail.com>
2023-08-27 17:29:07 +0300

    hw07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +14,81 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	currFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0o444)
+	if err != nil {
+		return err
+	}
+
+	defer currFile.Close()
+
+	err = validateSizeAndOffset(currFile, offset)
+	if err != nil {
+		return err
+	}
+
+	newFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	defer newFile.Close()
+
+	limit, err = prepareLimit(currFile, limit)
+	if err != nil {
+		return err
+	}
+
+	reader := io.NewSectionReader(currFile, offset, limit)
+
+	err = copyWithProgressBar(newFile, reader)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func validateSizeAndOffset(file *os.File, offset int64) error {
+	fi, err := file.Stat()
+	if err != nil {
+		return err
+	}
+
+	size := fi.Size()
+	if offset > size {
+		return ErrOffsetExceedsFileSize
+	}
+	if size == 0 {
+		return ErrUnsupportedFile
+	}
+
+	return nil
+}
+
+func prepareLimit(currFile *os.File, limit int64) (int64, error) {
+	fi, err := currFile.Stat()
+	if err != nil {
+		return 0, err
+	}
+
+	size := fi.Size()
+	if (limit > size) || (limit == 0) {
+		return size, nil
+	}
+
+	return limit, nil
+}
+
+func copyWithProgressBar(writer io.Writer, reader *io.SectionReader) error {
+	bar := pb.Full.Start64(reader.Size())
+	barReader := bar.NewProxyReader(reader)
+
+	_, err := io.Copy(writer, barReader)
+	if err != nil {
+		return err
+	}
+
+	bar.Finish()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,69 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		fromPath string
+		toPath   string
+		offset   int64
+		limit    int64
+		wantErr  assert.ErrorAssertionFunc
+	}{
+		{
+			name:     "positive, limit = 0",
+			fromPath: "testdata/input.txt",
+			toPath:   "new_file.txt",
+			offset:   0,
+			limit:    0,
+			wantErr:  assert.NoError,
+		},
+		{
+			name:     "positive, limit = 100000000",
+			fromPath: "testdata/input.txt",
+			toPath:   "new_file.txt",
+			offset:   0,
+			limit:    100000000,
+			wantErr:  assert.NoError,
+		},
+		{
+			name:     "positive, with offset",
+			fromPath: "testdata/input.txt",
+			toPath:   "new_file.txt",
+			offset:   100,
+			limit:    0,
+			wantErr:  assert.NoError,
+		},
+		{
+			name:     "offset exceeds file size",
+			fromPath: "testdata/input.txt",
+			toPath:   "new_file.txt",
+			offset:   1000000000,
+			limit:    0,
+			wantErr:  assert.Error,
+		},
+		{
+			name:     "unsupported file",
+			fromPath: "/dev/urandom",
+			toPath:   "new_file.txt",
+			offset:   1,
+			limit:    0,
+			wantErr:  assert.Error,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := Copy(tt.fromPath, tt.toPath, tt.offset, tt.limit)
+			tt.wantErr(t, err)
+		})
+		os.Remove(tt.toPath)
+	}
 }
