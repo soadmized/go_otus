c463dd0c9952a063864786489aabbc5c4027cc71
soadmized <soadmized1@gmail.com>
2023-09-10 15:34:47 +0300

    hw08 test fix

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -34,33 +34,39 @@ func Test_setEnvs(t *testing.T) {
 
 func TestRunCmd(t *testing.T) {
 	tests := []struct {
-		name string
-		cmd  []string
-		env  Environment
-		want int
+		name     string
+		cmd      []string
+		env      Environment
+		positive bool
+		want     int
 	}{
 		{
-			name: "positive",
-			cmd:  []string{"ls", "-h"},
-			env:  Environment{"FOO": EnvValue{Value: "foo"}, "BAR": EnvValue{Value: "bar"}},
-			want: 0,
+			name:     "positive",
+			cmd:      []string{"ls", "-h"},
+			env:      Environment{"FOO": EnvValue{Value: "foo"}, "BAR": EnvValue{Value: "bar"}},
+			positive: true,
+			want:     0,
 		},
 		{
 			name: "error - not found in path",
 			cmd:  []string{"ll", "-h"},
 			env:  Environment{"FOO": EnvValue{Value: "foo"}, "BAR": EnvValue{Value: "bar"}},
-			want: 2,
+			want: 0,
 		},
 		{
 			name: "error - exec command",
 			cmd:  []string{"ls", "-12"},
 			env:  Environment{"FOO": EnvValue{Value: "foo"}, "BAR": EnvValue{Value: "bar"}},
-			want: 1,
+			want: 0,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, RunCmd(tt.cmd, tt.env))
+			if tt.positive {
+				assert.Equal(t, tt.want, RunCmd(tt.cmd, tt.env))
+			} else {
+				assert.NotEqual(t, tt.want, RunCmd(tt.cmd, tt.env))
+			}
 		})
 	}
 }
