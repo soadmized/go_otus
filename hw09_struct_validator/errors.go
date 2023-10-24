43a04fbe4092fb0f4005fb1f3712ab387b55bad2
soadmized <soadmized1@gmail.com>
2023-10-24 10:20:49 +0300

    hw09 is completed

---
+++ hw09_struct_validator/errors.go
@@ -0,0 +1,64 @@
+package hw09structvalidator
+
+import (
+	"strings"
+
+	"github.com/pkg/errors" //nolint:depguard
+)
+
+type ValidationError struct {
+	Field string
+	Err   error
+}
+
+type ValidationErrors []ValidationError
+
+func (v ValidationErrors) Error() string {
+	strBuilder := strings.Builder{}
+
+	for _, ve := range v {
+		errMessage := ve.Err.Error()
+		if strBuilder.Len() != 0 {
+			errMessage += "\n\t"
+		}
+
+		strBuilder.WriteString(errMessage)
+	}
+
+	return strBuilder.String()
+}
+
+var (
+	ErrNotStruct                = errors.New("value is not a struct")
+	ErrIncorrectRule            = errors.New("unable to parse rule")
+	ErrIncorrectStringLen       = errors.New("incorrect string length")
+	ErrRegexString              = errors.New("string do not satisfy regexp")
+	ErrNoMatchingElementInSlice = errors.New("no matching element")
+	ErrIntLessThanMin           = errors.New("int value is less than min")
+	ErrIntMoreThanMax           = errors.New("int value is more than max")
+	ErrUnsupportedType          = errors.New("type is not supported")
+	ErrTagValueMustBeInt        = errors.New("tag value must be integer")
+	ErrIncorrectTagRegexPattern = errors.New("regexp pattern is invalid")
+)
+
+func (v ValidationErrors) Is(target error) bool {
+	var tErr ValidationErrors
+
+	if !errors.As(target, &tErr) {
+		return false
+	}
+
+	if len(v) != len(tErr) {
+		return false
+	}
+
+	for i := 0; i < len(tErr); i++ {
+		areFieldsEqual := v[i].Field == tErr[i].Field
+		areErrsEqual := errors.Is(v[i].Err, tErr[i].Err)
+		if !areFieldsEqual || !areErrsEqual {
+			return false
+		}
+	}
+
+	return true
+}
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,8 +2,9 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type UserRole string
@@ -36,25 +37,173 @@ type (
 	}
 )
 
-func TestValidate(t *testing.T) {
+func TestValidate(t *testing.T) { //nolint: funlen
 	tests := []struct {
+		name        string
 		in          interface{}
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			name: "positive",
+			in: Response{
+				Code: 200,
+				Body: "payload",
+			},
+			expectedErr: nil,
 		},
-		// ...
-		// Place your code here.
+		{
+			name:        "interface is not a struct",
+			in:          42,
+			expectedErr: ErrNotStruct,
+		},
+		{
+			name: "no validate tags",
+			in: Token{
+				Header:    []byte{1},
+				Payload:   []byte{2},
+				Signature: []byte{3},
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "invalid string len",
+			in:   App{Version: "1234"},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "Version",
+					Err:   ErrIncorrectStringLen,
+				},
+			},
+		},
+		{
+			name: "invalid min value",
+			in: User{
+				ID:     "c133763a-8bbd-4087-94ba-dfa9d5e8e805",
+				Age:    17,
+				Email:  "smth@mail.com",
+				Role:   "admin",
+				Phones: []string{"89003215467"},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "Age",
+					Err:   ErrIntLessThanMin,
+				},
+			},
+		},
+		{
+			name: "invalid max value",
+			in: User{
+				ID:     "c133763a-8bbd-4087-94ba-dfa9d5e8e805",
+				Age:    55,
+				Email:  "smth@mail.com",
+				Role:   "admin",
+				Phones: []string{"89003215467"},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "Age",
+					Err:   ErrIntMoreThanMax,
+				},
+			},
+		},
+		{
+			name: "not match to regexp",
+			in: User{
+				ID:     "c133763a-8bbd-4087-94ba-dfa9d5e8e805",
+				Age:    20,
+				Email:  "smth",
+				Role:   "admin",
+				Phones: []string{"89003215467"},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "Email",
+					Err:   ErrRegexString,
+				},
+			},
+		},
+		{
+			name: "invalid string slice",
+			in: User{
+				ID:     "c133763a-8bbd-4087-94ba-dfa9d5e8e805",
+				Age:    20,
+				Email:  "smth@mail.com",
+				Role:   "admin",
+				Phones: []string{"89003215"},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "Phones",
+					Err:   ErrIncorrectStringLen,
+				},
+			},
+		},
+		{
+			name: "invalid int slice",
+			in: struct {
+				integers []int `validate:"min:25"`
+			}{
+				integers: []int{20},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "integers",
+					Err:   ErrIntLessThanMin,
+				},
+			},
+		},
+		{
+			name: "invalid int tag",
+			in: struct {
+				integers []int `validate:"min:25.5"`
+			}{
+				integers: []int{20},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "integers",
+					Err:   ErrTagValueMustBeInt,
+				},
+			},
+		},
+		{
+			name: "invalid regexp tag",
+			in: struct {
+				strs []string `validate:"regexp:(qwerty]"`
+			}{
+				strs: []string{"123"},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "strs",
+					Err:   ErrIncorrectTagRegexPattern,
+				},
+			},
+		},
+		{
+			name: "invalid | tag",
+			in: struct {
+				strs []string `validate:"|len:22"`
+			}{
+				strs: []string{"123"},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "strs",
+					Err:   ErrIncorrectRule,
+				},
+			},
+		},
 	}
 
-	for i, tt := range tests {
-		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
 			tt := tt
 			t.Parallel()
 
-			// Place your code here.
-			_ = tt
+			err := Validate(tt.in)
+			assert.ErrorIs(t, err, tt.expectedErr)
 		})
 	}
 }
